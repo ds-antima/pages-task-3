ac6758b837dfa99753dd7f4402aa963e7985a18d
ds-antima <antima.agarwal@dotsquares.com>
2023-02-01 15:17:48 +0530

    new task

---
+++ src/components/layouts/header.tsx
@@ -0,0 +1,78 @@
+// import * as React from "react";
+// import Cta from "../commons/cta";
+
+//   type Link = {
+//     label: string;
+//     url: string;
+//   };
+
+//   const links: Link[] = [
+//     {
+//       label: "Home",
+//       url: "/",
+//     },
+//     {
+//       label: "About",
+//       url: "/about",
+//     },
+//     {
+//       label: "Menu",
+//       url: "/menu.html",
+//     },
+//     {
+//       label: "Locator",
+//       url: "/locator",
+//     }
+//   ];
+
+//   const Header = () => {
+//     const linkDoms = links.map((link) => (
+//       <div key={link.label}>
+//         <a href={link.url} >
+//           {link.label}
+//         </a>
+//       </div>
+//     ));
+
+//   return (
+//     <>
+//       <div className="centered-container">
+//         <nav className="py-3 flex items-center justify-between">
+//           <img
+//               src="https://a.mktgcdn.com/p/8esDUBrhKJnkaVztLihLsC3quv_5BjLFG9L6MJ0adcs/150x150.png">
+//             <div className="flex gap-x-10 text-lg font-semibold">{linkDoms}</div>
+//           <div className="hidden space-x-5 sm:block">
+//             <Cta buttonText="Order Pickup" url="#" style="primary-cta"></Cta>
+//             <Cta buttonText="Order Delivery" url="#" style="secondary-cta"></Cta>
+//           </div>
+//         </nav>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Header;
+import * as React from "react"
+export default function Header1(props:any){
+    console.log(props._site,"_sfcsgsf")
+
+return(
+    <>
+    <div className="flex flex-row" style={{width:"1500px",backgroundColor:"black",color:"white"}}>
+    {props._site.c_header.map((res:any)=>{
+        return(
+            <>
+            <div style={{width:"450px", margin:"45px" }}>
+            <ul>
+              <a href={res.link}>{res.label}</a>
+            </ul>
+            </div>
+            </>
+            )
+        })}
+        <a className="logo" href="\">
+        <img src={props._site.c_headerphoto.url}/></a>
+        </div>
+    </>
+)
+}
---
+++ src/config/answersHeadlessConfig.ts
@@ -0,0 +1,20 @@
+
+export const AnswerExperienceConfig = {
+  limit:3,
+  locale: "en",
+  apiKey : "d84607b3db6d93c6c7e4992be07ba799",
+  verticalKey : "locations",
+  experienceKey : "pages1",
+  experienceVersion: "STAGING",
+  locationRadius: 804672,
+  sessionTrackingEnabled: true,
+  endpoints: {
+    universalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/query",
+    verticalSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/query",
+    questionSubmission: "https://liveapi-sandbox.yext.com/v2/accounts/me/createQuestion",
+    universalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/autocomplete",
+    verticalAutocomplete: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/vertical/autocomplete",
+    filterSearch: "https://liveapi-sandbox.yext.com/v2/accounts/me/answers/filtersearch",
+
+  }
+}
